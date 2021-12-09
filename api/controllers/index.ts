7e8230ef7a2d726fb621e0a5628e3a28c32df8ce
Steeve Joseph <steevejoseph@knights.ucf.edu>
2021-12-09 09:01:13 -0500

    Hookup create-react-app to express

--- api/controllers/index.ts
+++ api/controllers/index.ts
@@ -1,6 +1,7 @@
+import path from "path";
 import { Handler } from "../../types";
 
 /* GET home page. */
 export const home: Handler = (req, res) => {
-  res.send("hello");
-}
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+};
