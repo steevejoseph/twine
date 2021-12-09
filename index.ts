65e877c479a4bfa67f32f771696541c4bada26ad
Steeve Joseph <steevejoseph@knights.ucf.edu>
2021-12-09 08:09:00 -0500

    Add dotenv, connect to mongoatlas

--- index.ts
+++ index.ts
@@ -1,17 +1,29 @@
+import "dotenv/config";
 import express from "express";
 import { routes } from "./api/routes/routes";
 import cookieParser from "cookie-parser";
 import path from "path";
 import logger from "morgan";
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(logger('dev'));
+const { MONGO_ATLAS_USER, MONGO_ATLAS_PASS } = process.env;
+
+mongoose.connect(
+  `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASS}@cluster0.wdzws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+);
+
+mongoose.connection.on("open", (ref) =>
+  console.log("Connected to mongo server.")
+);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 routes.forEach((route) => {
   const { method, path, middleware, handler } = route;
