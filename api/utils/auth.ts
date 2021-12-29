bfb629df45ab077da7c25b6b1147de06ac65009c
Steeve Joseph <steevejoseph@knights.ucf.edu>
2021-12-28 20:59:06 -0500

    Add util for pulling user from req

--- api/utils/auth.ts
+++ api/utils/auth.ts
@@ -1,5 +1,6 @@
 import { Request } from 'express';
 import jwt from 'jsonwebtoken';
+import { IUser } from '../models/user';
 
 /**
  * Decodes the token from a request's header, returns the corresponding object
@@ -11,8 +12,15 @@ export const decodeTokenFromRequest = (req: Request): any => {
   const token = req.headers.authorization?.split(' ')[1] as string;
   try {
     return jwt.verify(token, `${process.env.JWT_SECRET}`);
-  } catch (error) {
-    console.log(error);
-    return null;
+  } catch (e) {
+    console.log(e);
+    throw e;
   }
+};
+
+export const getUserFromRequest = (req: Request) => {
+  const decoded = decodeTokenFromRequest(req);
+  console.log(decoded);
+  const user: IUser = decoded.user;
+  return user;
 };
